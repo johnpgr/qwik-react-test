81105ff61e73723e835086fb6034a946f8c854b5
Joao Paulo <jp@greidinger.dev>
2022-10-24 09:55:27 -0300

    removed axios

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,73 +1,62 @@
-import { component$, useMount$, useStore } from "@builder.io/qwik";
+import { component$, Resource, useResource$ } from "@builder.io/qwik";
 import { getAll } from "~/lib/todos";
 import { Todo } from "~/types/Todo";
 import { AiOutlineCheck, AiOutlineClose } from "react-icons/ai";
 import { BsThreeDotsVertical } from "react-icons/bs";
 import { qwikify$ } from "@builder.io/qwik-react";
+import { CgSpinner } from "react-icons/cg";
 
 export const CheckIcon = qwikify$(AiOutlineCheck);
 export const CloseIcon = qwikify$(AiOutlineClose);
 export const MenuButton = qwikify$(BsThreeDotsVertical);
+export const Spinner = qwikify$(CgSpinner);
 
 export const TodoList = component$(() => {
-    const store = useStore<{
-        todos: Todo[] | null;
-        isLoading: boolean;
-        error?: string;
-    }>({
-        todos: [] as Todo[],
-        isLoading: false,
-    });
-
-    useMount$(async () => {
-        store.isLoading = true;
-        await getAll()
-            .then((todos) => {
-                store.todos = todos;
-                store.isLoading = false;
-            })
-            .catch((error) => {
-                store.error = error.message;
-                store.isLoading = false;
-            });
-    });
+    const data = useResource$<Todo[]>(async () => getAll());
 
     return (
         <div>
-            {store.isLoading && <div>Loading...</div>}
-            {store.error && <div>{store.error}</div>}
-            {store.todos && (
-                <ul className="grid grid-cols-4 gap-4 mt-4">
-                    {store.todos.map((todo) => (
-                        <li
-                            className="p-4 rounded bg-white drop-shadow-md flex"
-                            key={todo.id}
-                        >
-                            <div className="flex flex-col h-full">
-                                <span className="font-semibold">
-                                    {todo.title}
-                                </span>
-                                <span className="text-sm text-zinc-500">
-                                    {todo.description}
-                                </span>
-                                <span className="mt-auto text-sm">
-                                    {new Date(todo.createdAt).toLocaleString()}
-                                </span>
-                            </div>
-                            <div className="flex flex-col items-center mt-1 justify-between">
-                                <button>
-                                    <MenuButton className="text-xl" />
-                                </button>
-                                {todo.completed ? (
-                                    <CheckIcon className="text-2xl text-green-500" />
-                                ) : (
-                                    <CloseIcon className="text-2xl text-red-500" />
-                                )}
-                            </div>
-                        </li>
-                    ))}
-                </ul>
-            )}
+            <Resource
+                value={data}
+                onPending={() => (
+                    <Spinner className="text-blue-500 text-2xl animate-spin" />
+                )}
+                onRejected={(error) => <div>Error: {error.message}</div>}
+                onResolved={(todos) => (
+                    <ul className="grid grid-cols-4 gap-4 mt-4">
+                        {todos.map((todo) => (
+                            <li
+                                className="p-4 rounded bg-white drop-shadow-md flex"
+                                key={todo.id}
+                            >
+                                <div className="flex flex-col h-full">
+                                    <span className="font-semibold">
+                                        {todo.title}
+                                    </span>
+                                    <span className="text-sm text-zinc-500">
+                                        {todo.description}
+                                    </span>
+                                    <span className="mt-auto text-sm">
+                                        {new Date(
+                                            todo.createdAt
+                                        ).toLocaleString()}
+                                    </span>
+                                </div>
+                                <div className="flex flex-col items-center mt-1 justify-between">
+                                    <button>
+                                        <MenuButton className="text-xl" />
+                                    </button>
+                                    {todo.completed ? (
+                                        <CheckIcon className="text-2xl text-green-500" />
+                                    ) : (
+                                        <CloseIcon className="text-2xl text-red-500" />
+                                    )}
+                                </div>
+                            </li>
+                        ))}
+                    </ul>
+                )}
+            />
         </div>
     );
 });
--- src/lib/todos.ts
+++ src/lib/todos.ts
@@ -1,22 +1,5 @@
-import axios from "axios";
-import { Todo } from "~/types/Todo";
-
 export async function getAll() {
-    return axios
-        .request<{ results: Todo[] }>({
-            url: "https://start.honotest123.workers.dev/todos/list-all",
-            method: "GET",
-        })
-        .then((res) => {
-            return res.data.results;
-        });
-}
-
-export async function deleteOne(id: string) {
-    return axios
-        .post(`https://start.honotest123.workers.dev/todos/${id}`)
-        .then((res) => {
-            console.log(res);
-            return res.data;
-        });
+    return fetch("https://start.honotest123.workers.dev/todos/list-all").then(
+        async (res) => res.json().then((data) => data.results)
+    );
 }
